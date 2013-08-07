61f6ef5534ea804aea88467e9470b3b8f02c79f4
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-07 16:38:39 -0500

    Long url works. Unique short url works. click counter works.

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -7,13 +7,17 @@ end
 get '/:short_url' do
   #redirect to appropriate long url
   u = Url.find_by_short_url(params[:short_url])
+  u.update(click_counter: u.click_counter + 1)
   puts u.long_url
+  redirect to "#{u.long_url}"
+
 end
 
 post '/urls' do
   #create a new Url
   p params
   @url = Url.find_by_long_url(params[:long_url])
-  @url = Url.create(long_url: params[:long_url]) unless url
+  @url = Url.create(long_url: params[:long_url]) unless @url
+  p @url
   erb :result
 end
