34981de2594a73d11f814fb718c389ec0ea7bbc9
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-07 16:59:11 -0500

    mvp complete

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -4,20 +4,28 @@ get '/' do
   erb :index
 end
 
+
+get '/urls/:id' do
+  # displays specific url entry
+  @url = Url.find(params[:id])
+  erb :result
+end
+
 get '/:short_url' do
-  #redirect to appropriate long url
+  # redirect to appropriate long url
   u = Url.find_by_short_url(params[:short_url])
   u.update(click_counter: u.click_counter + 1)
   puts u.long_url
   redirect to "#{u.long_url}"
-
 end
 
 post '/urls' do
-  #create a new Url
-  p params
-  @url = Url.find_by_long_url(params[:long_url])
+  # create a new Url
   @url = Url.create(long_url: params[:long_url]) unless @url
-  p @url
-  erb :result
+  if @url.valid?
+    redirect to "/urls/#{@url.id}"
+  else
+    erb :index
+  end
 end
+
